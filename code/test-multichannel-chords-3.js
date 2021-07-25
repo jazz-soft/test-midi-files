d6019d44a88e33c0b4109f8bd4461991ffdf00b7
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2021-07-25 09:30:27 +0100

    Tracks and channels

---
+++ code/test-multichannel-chords-3.js
@@ -0,0 +1,51 @@
+var TEST = require('..');
+var JZZ = require('jzz');
+require('jzz-midi-smf')(JZZ);
+
+var smf = new JZZ.MIDI.SMF(1);
+var trk0 = new JZZ.MIDI.SMF.MTrk();
+smf.push(trk0);
+trk0.smfSeqName('Multi-channel chords Test')
+  .smfCopyright('https://jazz-soft.net')
+  .smfText('A format 1 file with two tracks sharing a channel.\n')
+  .smfText(' Now you must hear a chord at C5!')
+  .noteOn(0, 'C5', 127).tick(96).noteOff(0, 'C5')
+  .smfText(' Now you must hear a chord at D5!')
+  .noteOn(0, 'D5', 127).tick(96).noteOff(0, 'D5')
+  .smfText(' Now you must hear a chord at E5!')
+  .noteOn(0, 'E5', 127).tick(96).noteOff(0, 'E5')
+  .smfText(' Now you must hear a chord at F5!')
+  .noteOn(0, 'F5', 127).tick(96).noteOff(0, 'F5')
+  .smfText(' Now you must hear a chord at G5!')
+  .noteOn(0, 'G5', 127).tick(96).noteOff(0, 'G5')
+  .smfText(' Now you must hear a chord at A5!')
+  .noteOn(0, 'A5', 127).tick(96).noteOff(0, 'A5')
+  .smfText(' Now you must hear a chord at B5!')
+  .noteOn(0, 'B5', 127).tick(96).noteOff(0, 'B5')
+  .smfText(' Now you must hear a chord at C6!')
+  .noteOn(0, 'C6', 127).tick(96).noteOff(0, 'C6')
+  .smfText('Thank you!');
+var trk1 = new JZZ.MIDI.SMF.MTrk();
+smf.push(trk1);
+trk1
+  .noteOn(1, 'E5', 127).tick(96).noteOff(1, 'E5')
+  .noteOn(1, 'F5', 127).tick(96).noteOff(1, 'F5')
+  .noteOn(1, 'G5', 127).tick(96).noteOff(1, 'G5')
+  .noteOn(1, 'A5', 127).tick(96).noteOff(1, 'A5')
+  .noteOn(1, 'B5', 127).tick(96).noteOff(1, 'B5')
+  .noteOn(1, 'C6', 127).tick(96).noteOff(1, 'C6')
+  .noteOn(1, 'D6', 127).tick(96).noteOff(1, 'D6')
+  .noteOn(1, 'E6', 127).tick(96).noteOff(1, 'E6');
+var trk2 = new JZZ.MIDI.SMF.MTrk();
+smf.push(trk2);
+trk2
+    .noteOn(0, 'G5', 127).tick(96).noteOff(0, 'G5')
+    .noteOn(0, 'A5', 127).tick(96).noteOff(0, 'A5')
+    .noteOn(0, 'B5', 127).tick(96).noteOff(0, 'B5')
+    .noteOn(0, 'C6', 127).tick(96).noteOff(0, 'C6')
+    .noteOn(0, 'D6', 127).tick(96).noteOff(0, 'D6')
+    .noteOn(0, 'E6', 127).tick(96).noteOff(0, 'E6')
+    .noteOn(0, 'F6', 127).tick(96).noteOff(0, 'F6')
+    .noteOn(0, 'G6', 127).tick(96).noteOff(0, 'G6');
+TEST.write(smf);
+TEST.play(smf);
