49cdda28bab8ed5ded605b4e154b3bdc0e4454cf
Sema <jazz-dev@jazz-soft.net>
2022-05-16 23:15:17 -0400

    Minor fix

--- code/test-2-tracks-type-2.js
+++ code/test-2-tracks-type-2.js
@@ -20,14 +20,14 @@ trk.smfSeqName('Standard MIDI file type 2')
 trk = new JZZ.MIDI.SMF.MTrk();
 smf.push(trk);
 trk.smfText('Track 2').tick(96)
-  .noteOn(1, 'C#5', 127).tick(96).noteOff(1, 'C5#')
-  .noteOn(1, 'D#5', 127).tick(96).noteOff(1, 'D5#')
-  .noteOn(1, 'E#5', 127).tick(96).noteOff(1, 'E5#')
-  .noteOn(1, 'F#5', 127).tick(96).noteOff(1, 'F5#')
-  .noteOn(1, 'G#5', 127).tick(96).noteOff(1, 'G5#')
-  .noteOn(1, 'A#5', 127).tick(96).noteOff(1, 'A5#')
-  .noteOn(1, 'B#5', 127).tick(96).noteOff(1, 'B5#')
-  .noteOn(1, 'C#6', 127).tick(96).noteOff(1, 'C6#')
+  .noteOn(1, 'C#5', 127).tick(96).noteOff(1, 'C#5')
+  .noteOn(1, 'D#5', 127).tick(96).noteOff(1, 'D#5')
+  .noteOn(1, 'E#5', 127).tick(96).noteOff(1, 'E#5')
+  .noteOn(1, 'F#5', 127).tick(96).noteOff(1, 'F#5')
+  .noteOn(1, 'G#5', 127).tick(96).noteOff(1, 'G#5')
+  .noteOn(1, 'A#5', 127).tick(96).noteOff(1, 'A#5')
+  .noteOn(1, 'B#5', 127).tick(96).noteOff(1, 'B#5')
+  .noteOn(1, 'C#6', 127).tick(96).noteOff(1, 'C#6')
   .smfText('Thank you!');
 
 TEST.write(smf);
