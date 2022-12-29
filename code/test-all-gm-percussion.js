29109712300b2c965288d2ccd52c9ed9ef7dfb95
Sema <jazz-dev@jazz-soft.net>
2022-12-28 20:59:16 -0500

    Minor fixes

--- code/test-all-gm-percussion.js
+++ code/test-all-gm-percussion.js
@@ -6,10 +6,10 @@ require('jzz-midi-gm')(JZZ);
 var smf = new JZZ.MIDI.SMF(0, 96);
 var trk = new JZZ.MIDI.SMF.MTrk();
 smf.push(trk);
-trk = trk.sxGM(2).smfSeqName('All GM Percussion')
+trk = trk.smfSeqName('All GM Percussion')
   .smfCopyright('https://jazz-soft.net')
   .smfText('Playing all General MIDI percussion sounds.\n')
-  .ch(9);
+  .sxGM(2).ch(9);
 
 for (var n = 27; n < 88; n++) {
   trk = trk.smfText(n + ' ' + JZZ.MIDI.percussionName(n) + (n < 35 || n > 81 ? ' (GM2)' : ''))
